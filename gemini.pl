cf8f6d4975ecc296e320b02348c9b09c89048479
Dan DeWaele <dantidote@gmail.com>
2017-03-30 22:04:42 -0400

    config file

--- gemini.pl
+++ gemini.pl
@@ -4,27 +4,24 @@ use REST::Client;
 use JSON;
 use MIME::Base64;
 use Data::Dumper;
+use Config::Simple;
 use Digest::SHA qw(hmac_sha384_hex);
+use Sys::Syslog;
 
-my $amountToBuyInUsd = '3.33';
+my $configFile = 'gemini.config';
 
-#my $host = 'https://api.sandbox.gemini.com';
-#my $secretFile = '/media/storage/Backup/gemini_sandbox_api_secret';
-#my $apiKeyFile = '/media/storage/Backup/gemini_sandbox_api_key';
+my $debug = 1;
 
-my $host = 'https://api.gemini.com';
-my $secretFile = '/media/storage/Backup/gemini_api_secret';
-my $apiKeyFile = '/media/storage/Backup/gemini_api_key';
+openlog("GEMINI", 'ndelay,pid', "local0");
 
-open( my $fh, '<', $secretFile ) or die "Can't open $secretFile: $!";
-my $apiSecret = <$fh>;
-chomp $apiSecret;
-close $fh;
+Config::Simple->import_from($configFile, \%config);
 
-open( my $fh, '<', $apiKeyFile ) or die "Can't open $apiKeyFile: $!";
-my $apiKey = <$fh>;
-chomp $apiKey;
-close $fh;
+my $amountToBuyInUsd = $config{'amountToBuyInUsd'};
+my $address = $config{'address'};
+my $host = $config{'host'};
+my $apiSecret = $config{'apiSecret'};
+my $apiKey = $config{'apiKey'};
+my $maxBTC = $config{'maxBTCToKeep'};
 
 checkFunds();
 buyBtc();
@@ -35,6 +32,40 @@ sub getNonce(){
  chomp $nonce;
  return $nonce;
 
+}
+
+
+sub withdrawBTC{
+
+ my $amt = shift;
+ my $nonce = getNonce();
+
+ if($debug){ print "Withdrawing $amt BTC\n" }
+
+ my $payload = qq(
+ {
+    "request": "/v1/withdraw/btc",
+    "nonce": $nonce,
+    "address": "$address",
+    "amount": "$amt"
+ }
+ );
+
+ if($debug){ print "$payload\n"; }
+ $enc_payload = encode_base64($payload,"");
+
+ my $client = REST::Client->new();
+ $client->setHost($host);
+
+ $client->addHeader('X-GEMINI-APIKEY', $apiKey);
+ $client->addHeader('X-GEMINI-PAYLOAD', $enc_payload);
+ $client->addHeader('X-GEMINI-SIGNATURE', hmac_sha384_hex($enc_payload,$apiSecret));
+
+ $client->POST('/v1/withdraw/btc');
+
+ my $res = $client->responseContent();
+
+ print $res;
 }
 
 sub checkFunds(){
@@ -64,16 +95,29 @@ sub checkFunds(){
 
  my $wallets = from_json( $res );
  
- #Can't guarantee the array order. Must iterate through to find the USD wallet.
+ #Can't guarantee the array order. Must iterate through to find the correct wallet.
  foreach $wallet (@$wallets){
   if( $wallet->{currency} eq "USD" ){
+    if($debug){ print "Current USD Balance: $wallet->{available}\n"};
     if( $wallet->{available} > $amountToBuyInUsd ){
+      if($debug){ print "We have enough USD to buy BTC\n" }
       return 0;
     }
     else{
       die "not enough USD";
     }
   }
+  elsif( $wallet->{currency} eq "BTC" ){
+    if($debug){ print "Current BTC Balance: $wallet->{available}\n"};
+    if( $wallet->{available} > $maxBTC ){
+      if($debug){ print "Balance is greater than threshold.\n" }
+      withdrawBTC( $wallet->{available} );
+    }
+    else{
+      if($debug){ print "Not withdrawing BTC because your balance isn't greater than $maxBTC\n" }
+    }
+  }
+
  }
 }
 
